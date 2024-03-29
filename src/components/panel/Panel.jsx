23574b21e5f0902d43b1242145dfcc4f62ae1831
Antoine Bonin <antoinebonin99@gmail.com>
2024-03-29 17:40:34 +0100

    fix: affichage programme

--- src/components/panel/Panel.jsx
+++ src/components/panel/Panel.jsx
@@ -3,8 +3,16 @@ import Timer from "./timer/Timer";
 import './Panel.css'
 import Match from "./match/Match";
 import Programme from "./programme/programme";
+import { useState } from "react";
 
 export default function Panel () {
+
+    const [progOpen, setProgOpen] = useState(false);
+
+    const handleProgOpen = () => {
+        console.log(progOpen);
+        setProgOpen(!progOpen)
+    }
 
     return (
         <>
@@ -14,7 +22,8 @@ export default function Panel () {
             <Team index={1}/>
             <Timer/>
         </div>
-        <Programme/>
+        <button onClick={() => handleProgOpen()}>{progOpen ? "Fermer" : "Ouvrir"} le programme</button>
+        { progOpen && <Programme/>}
         </>
     )
 }
